299597cc4c9cf2669d0ee624e9dc6e671a4f2893
BurnettMiao <searchfor1987@gmail.com>
2023-10-16 17:06:50 +0800

    about section finish

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const scrollRevealOption = {
   duration: 1000,
 };
 
-// header container
+// header section
 ScrollReveal().reveal('.header__container img', {
   duration: 1000,
 });
@@ -22,4 +22,30 @@ ScrollReveal().reveal('.header__container p', {
 ScrollReveal().reveal('.header__btns', {
   ...scrollRevealOption,
   delay: 1500,
+});
+
+// about section
+ScrollReveal().reveal('.about__image img', {
+  ...scrollRevealOption,
+  origin: 'left',
+});
+
+ScrollReveal().reveal('.about__content h3', {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+ScrollReveal().reveal('.about__content .section__header', {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+
+ScrollReveal().reveal('.about__content .section__subheader', {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+
+ScrollReveal().reveal('.about__content .about__grid', {
+  ...scrollRevealOption,
+  delay: 2000,
 });
